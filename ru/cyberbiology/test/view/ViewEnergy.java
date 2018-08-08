c70f0c33327154d88dae079d3d95120a78d0f526
Sergey Sokolov <xlamserg@gmail.com>
2018-08-08 09:32:53 +0400

    ViewEnergy наследует от абстрактного View

--- ru/cyberbiology/test/view/ViewEnergy.java
+++ ru/cyberbiology/test/view/ViewEnergy.java
@@ -1,22 +1,14 @@
 package ru.cyberbiology.test.view;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
 
-import javax.swing.JPanel;
 import ru.cyberbiology.test.Bot;
+import ru.cyberbiology.test.prototype.view.View;
 
-import ru.cyberbiology.test.World;
-import ru.cyberbiology.test.prototype.view.IView;
-
-public class ViewEnergy implements IView
+public class ViewEnergy extends View
 {
 
-	public ViewEnergy()
-	{
-		// TODO Auto-generated constructor stub
-	}
+	public ViewEnergy()	{}
 
 	@Override
 	public String getName()
@@ -25,63 +17,28 @@ public class ViewEnergy implements IView
 	}
 
     @Override
-    public Image paint(World world,JPanel canvas) {
-    	int w = canvas.getWidth();
-    	int h = canvas.getHeight();
-    	//Создаем временный буфер для рисования
-    	Image buf = canvas.createImage(w, h);
-    	//подеменяем графику на временный буфер
-    	Graphics g = buf.getGraphics();
+    public Color getBotColor(Bot bot) {
 
-        g.drawRect(0, 0, world.width * World.BOTW + 1, world.height * World.BOTH + 1);
+        Color color = Color.WHITE;
 
-        world.pests = 0;
-        world.pestGenes = 0;
-        for (int y = 0; y < world.height; y++) {
-            for (int x = 0; x < world.width; x++) {
-                Bot bot = world.matrix[x][y];
-                if (bot == null) {
-                    g.setColor(Color.WHITE);
-                    g.fillRect(x * World.BOTW,y * World.BOTH, World.BOTW, World.BOTH);
-                } else if ((bot.alive == 1) || (bot.alive == 2)) {
-                    g.setColor(new Color(200, 200, 200));
-                    g.fillRect(x * World.BOTW, y * World.BOTH, World.BOTW, World.BOTH);
-                } else if (bot.alive == 3) {
-                    g.setColor(Color.BLACK);
-                    g.drawRect(x * World.BOTW, y * World.BOTH, World.BOTW, World.BOTH);
+        if (bot == null) {
+            // Color.WHITE
+        } else if ((bot.alive == bot.LV_ORGANIC_SINK) || (bot.alive == bot.LV_ORGANIC_HOLD)) {
+            color = new Color(200, 200, 200);
+        } else if (bot.alive == bot.LV_ALIVE) {
+            // черный цвет для умерших в данном пересчете ботов
+            int colorRed = 0;
+            int colorGreen = 0;
+            int colorBlue = 0;
 
-                    // черный цвет для умерших в данном пересчете ботов
-                    int colorRed = 0;
-                    int colorGreen = 0;
-                    int colorBlue = 0;
-
-                    // цвет выживших ботов в зависимости от энергии
-                    if (bot.health > 0) {
-                        colorRed = 255;
-                        colorGreen = (int) Math.round(Math.abs(bot.health * 0.255 - 255));
-                    }
-
-                    // исключения по цвету помогают выявить ботов с аномальным
-                    // количеством энергии (здоровья)
-                    try {
-                        g.setColor(new Color(colorRed, colorGreen, colorBlue));
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                        System.out.println("ViewEnergy.paint(): " +
-                            "green=" + colorGreen + " " +
-                            "health=" + bot.health
-                        );
-                    }
-
-                    if (bot.pest > 0) {
-                        world.pestGenes += world.matrix[x][y].pest;
-                        world.pests++;
-                    }
-
-                    g.fillRect(x * World.BOTW + 1, y * World.BOTH + 1,World.BOTW-1, World.BOTH-1);
-                }
+            // цвет живых ботов в зависимости от энергии
+            if (bot.health > 0) {
+                colorRed = 255;
+                colorGreen = (int) Math.round(Math.abs(bot.health * 0.255 - 255));
             }
+
+            color = new Color(colorRed, colorGreen, colorBlue);
         }
-        return buf;
+        return color;
     }
 }
