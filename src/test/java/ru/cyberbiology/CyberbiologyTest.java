7b706f89a38ed880c655f252997692c1909d8f05
Sergey Sokolov <xlamserg@gmail.com>
2018-10-31 19:14:59 +0400

    Добавление JUnit 4.12

---
+++ src/test/java/ru/cyberbiology/CyberbiologyTest.java
@@ -0,0 +1,39 @@
+package ru.cyberbiology;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sergey Sokolov <xlamserg@gmail.com>
+ */
+public class CyberbiologyTest {
+
+    public CyberbiologyTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+     @Test
+     public void hello() {
+         assertTrue(true);
+     }
+}
