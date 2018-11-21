332925eb6b00c08496150dbf129b0eca37a8e777
Sergey Sokolov <xlamserg@gmail.com>
2018-11-21 20:58:46 +0400

    wip: новый диалог настроек готов, но пока не активен

--- src/main/java/ru/cyberbiology/SettingsDialog.java
+++ src/main/java/ru/cyberbiology/SettingsDialog.java
@@ -1,6 +1,11 @@
 package ru.cyberbiology;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.JCheckBox;
 import ru.cyberbiology.util.ProjectProperties;
 
 /**
@@ -10,11 +15,16 @@ import ru.cyberbiology.util.ProjectProperties;
 public class SettingsDialog extends javax.swing.JDialog {
 
     private final ProjectProperties properties;
+    private final Map<String, String> changedSettings = new HashMap();
 
     public SettingsDialog(Frame parent, boolean modal) {
         super(parent, modal);
         properties = ProjectProperties.getInstance();
         initComponents();
+        loadSettings();
+    }
+
+    private void loadSettings() {
         cbMultiCell.setSelected(properties.getBoolean("EnableMultiCell"));
         cbRelativeByEnergy.setSelected(properties.getBoolean("EnableRelativeByEnergy"));
         cbRelativeByMinerals.setSelected(properties.getBoolean("EnableRelativeByMinerals"));
@@ -31,6 +41,7 @@ public class SettingsDialog extends javax.swing.JDialog {
 
         tabbedPane = new javax.swing.JTabbedPane();
         panelSettingsGeneral = new javax.swing.JPanel();
+        labelDevNote = new javax.swing.JLabel();
         panelSettingaBots = new javax.swing.JPanel();
         cbMultiCell = new javax.swing.JCheckBox();
         cbRelativeByEnergy = new javax.swing.JCheckBox();
@@ -45,25 +56,41 @@ public class SettingsDialog extends javax.swing.JDialog {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Параметры");
 
+        labelDevNote.setText("Здесь будут общие настройки");
+
         javax.swing.GroupLayout panelSettingsGeneralLayout = new javax.swing.GroupLayout(panelSettingsGeneral);
         panelSettingsGeneral.setLayout(panelSettingsGeneralLayout);
         panelSettingsGeneralLayout.setHorizontalGroup(
             panelSettingsGeneralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 310, Short.MAX_VALUE)
+            .addGroup(panelSettingsGeneralLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(labelDevNote)
+                .addContainerGap(127, Short.MAX_VALUE))
         );
         panelSettingsGeneralLayout.setVerticalGroup(
             panelSettingsGeneralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 109, Short.MAX_VALUE)
+            .addGroup(panelSettingsGeneralLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(labelDevNote)
+                .addContainerGap(84, Short.MAX_VALUE))
         );
 
         tabbedPane.addTab("Общие", panelSettingsGeneral);
 
         cbMultiCell.setText("многоклеточность");
+        cbMultiCell.setEnabled(false);
+        cbMultiCell.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                cbMultiCellStateChanged(evt);
+            }
+        });
 
         cbRelativeByEnergy.setText("родство по энергии");
+        cbRelativeByEnergy.setEnabled(false);
 
         cbRelativeByMinerals.setText("родство по минералам");
         cbRelativeByMinerals.setToolTipText("");
+        cbRelativeByMinerals.setEnabled(false);
 
         javax.swing.GroupLayout panelSettingaBotsLayout = new javax.swing.GroupLayout(panelSettingaBots);
         panelSettingaBots.setLayout(panelSettingaBotsLayout);
@@ -75,7 +102,7 @@ public class SettingsDialog extends javax.swing.JDialog {
                     .addComponent(cbMultiCell)
                     .addComponent(cbRelativeByEnergy)
                     .addComponent(cbRelativeByMinerals))
-                .addContainerGap(137, Short.MAX_VALUE))
+                .addContainerGap(145, Short.MAX_VALUE))
         );
         panelSettingaBotsLayout.setVerticalGroup(
             panelSettingaBotsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -96,9 +123,19 @@ public class SettingsDialog extends javax.swing.JDialog {
         buttonPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.RIGHT));
 
         buttonOk.setText("ОК");
+        buttonOk.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonOkActionPerformed(evt);
+            }
+        });
         buttonPanel.add(buttonOk);
 
         buttonCancel.setText("Отмена");
+        buttonCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonCancelActionPerformed(evt);
+            }
+        });
         buttonPanel.add(buttonCancel);
 
         getContentPane().add(buttonPanel, java.awt.BorderLayout.SOUTH);
@@ -108,6 +145,20 @@ public class SettingsDialog extends javax.swing.JDialog {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCancelActionPerformed
+        dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    }//GEN-LAST:event_buttonCancelActionPerformed
+
+    private void buttonOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonOkActionPerformed
+        saveAndClose();
+    }//GEN-LAST:event_buttonOkActionPerformed
+
+    private void cbMultiCellStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_cbMultiCellStateChanged
+        JCheckBox cb = (JCheckBox) evt.getSource();
+        changedSettings.put("EnableMultiCell", cb.isSelected() ? "true" : "false");
+    }//GEN-LAST:event_cbMultiCellStateChanged
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonCancel;
     private javax.swing.JButton buttonOk;
@@ -118,12 +169,23 @@ public class SettingsDialog extends javax.swing.JDialog {
     private javax.swing.Box.Filler filler1;
     private javax.swing.Box.Filler filler2;
     private javax.swing.Box.Filler filler3;
+    private javax.swing.JLabel labelDevNote;
     private javax.swing.JPanel panelSettingaBots;
     private javax.swing.JPanel panelSettingsGeneral;
     private javax.swing.JTabbedPane tabbedPane;
     // End of variables declaration//GEN-END:variables
 
     void showSettingsDialog() {
+        setLocationRelativeTo(getParent());
         setVisible(true);
     }
+
+    private void saveAndClose() {
+        Set<Map.Entry<String, String>> changedSet = changedSettings.entrySet();
+        for (Map.Entry<String, String> entry: changedSet) {
+            properties.setProperty(entry.getKey(), entry.getValue());
+        }
+        dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    }
+
 }
